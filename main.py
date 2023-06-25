5d37439e1e191b236c1129e542385defc4660a15
Artem <kozelstudy@gmail.com>
2023-06-25 11:22:29 +0200

    Update

--- main.py
+++ main.py
@@ -5,6 +5,5 @@ class MyClient(discord.Client):
         print('Loged as{0}!'.format(self.user))
     async def on_message(self, message):
         print('Message from{0.author}: {0.content}'.format(message))
-
-client = MyClient()
+client = MyClient(intents=discord.Intents.default())
 client.run('MTEyMjI1ODk0NzkzNjYyMDY1Ng.GT9O9Y.hj-mPoLlVAxXMP4mZP5c5pt1ZtlGpT-lZ_zQJs')
